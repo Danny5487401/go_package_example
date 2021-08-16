363d40721c8c5c4e741e3bfecaef1680a5c18bb5
xiaxin <xiaxin@qimao.com>
2021-08-16 13:38:21 +0800

    docs(grpc文档): grpc源码补充

--- 08_grpc/01_grpc_helloworld/client/client.go
+++ 08_grpc/01_grpc_helloworld/client/client.go
@@ -8,15 +8,19 @@ import (
 )
 
 func main() {
+	//1.新建一个conn连接，
 	conn, err := grpc.Dial("127.0.0.1:9000", grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
 	c := proto.NewGreeterClient(conn)
+
 	r, err := c.SayHello(context.Background(), &proto.HelloRequest{
 		Name: "danny",
-	})
+	}) // 核心调用的是 Invoke 方法，具体实现要看grpc.ClientConn中
+	// grpc.ClientConn中实现了Invoke方法，在call.go文件中，详情都在invoke
+
 	if err != nil {
 		panic(err)
 	}
--- 08_grpc/07_grpc_error/server/server.go
+++ 08_grpc/07_grpc_error/server/server.go
@@ -23,7 +23,6 @@ func (s *Server) SayHello(ctx context.Context, request *proto.HelloRequest) (*pr
 
 func main() {
 	g := grpc.NewServer()
-
 	proto.RegisterGreeterServer(g, &Server{})
 	lis, err := net.Listen("tcp", "127.0.0.1:9000")
 	if err != nil {
@@ -31,3 +30,24 @@ func main() {
 	}
 	_ = g.Serve(lis)
 }
+
+/*
+gRPC的错误码，原代码见链接，我们大概了解其原因即可：
+	OK 正常
+	Canceled 客户端取消
+	Unknown 未知
+	InvalidArgument 未知参数
+	DeadlineExceeded 超时
+	NotFound 未找到资源
+	AlreadyExists 资源已经创建
+	PermissionDenied 权限不足
+	ResourceExhausted 资源耗尽
+	FailedPrecondition 前置条件不满足
+	Aborted 异常退出
+	OutOfRange 超出范围
+	Unimplemented 未实现方法
+	Internal 内部问题
+	Unavailable 不可用状态
+	DataLoss 数据丢失
+	Unauthenticated 未认证
+*/
