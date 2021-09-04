744394efe0dcf44dd6b0ec92ec138c6782ee81a4
xiaxin <xiaxin@qimao.com>
2021-09-04 16:28:23 +0800

    docs(grpc源码): grpc源码关于客户端的补充

--- 08_grpc/scripts/proto.sh
+++ 08_grpc/scripts/proto.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # 生成golang代码
-#工具 gogoprotobuf
+# 工具 gogoprotobuf
 # protoc --proto_path=${GOPATH}/src:. --gogofaster_out=plugins=grpc:. 04_jsonpb/proto/member.proto
 
 #protobuf
-protoc  --gofast_out=plugins=grpc:./04_jsonpb/proto 04_jsonpb/proto/member.proto
-protoc-go-inject-tag -input=04_jsonpb/proto/member.pb.go
+protoc  --gofast_out=plugins=grpc:./01_grpc_helloworld/proto ./01_grpc_helloworld/proto/helloworld.proto
+protoc-go-inject-tag -input=./01_grpc_helloworld/proto/helloworld.pb.go
 
