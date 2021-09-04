bfa37465a29e227f718bb66a7cfebf4c776e2195
xiaxin <xiaxin@qimao.com>
2021-09-04 12:07:27 +0800

    feat(反序列化): jsonpb.Unmarshaler自定义允许忽略未知字段

---
+++ 08_grpc/scripts/proto.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# 生成golang代码
+#工具 gogoprotobuf
+# protoc --proto_path=${GOPATH}/src:. --gogofaster_out=plugins=grpc:. 04_jsonpb/proto/member.proto
+
+#protobuf
+protoc  --gofast_out=plugins=grpc:./04_jsonpb/proto 04_jsonpb/proto/member.proto
+protoc-go-inject-tag -input=04_jsonpb/proto/member.pb.go
+
