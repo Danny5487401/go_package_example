abe56b2f9d0e82cbe61fda61c63dcf2a53128fa1
xiaxin <xiaxin@qimao.com>
2021-11-26 09:10:21 +0800

    docs(consul): 注册中心基本介绍

--- 06_get_available_ip_port/get_ip/main.go
+++ 06_get_available_ip_port/get_ip/main.go
@@ -5,25 +5,28 @@ import (
 	"net"
 	"strings"
 )
+
 // 获取对外ip
-func GetOutboundIP()(ip string,err error){
-	conn,err :=net.Dial("udp","8.8.8.8:80")
-	if err !=nil{
+func GetOutboundIP() (ip string, err error) {
+	// 8.8.8.8:google, 114.114.114.114是国内移动、电信和联通通用的DNS
+	//conn, err := net.Dial("udp", "8.8.8.8:80")
+	conn, err := net.Dial("udp", "114.114.114.114:80")
+	if err != nil {
 		return
 	}
 	defer conn.Close()
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 	fmt.Println(localAddr.String())
-	fmt.Println(localAddr.IP)
-	fmt.Println(localAddr.Port)
-	ip = strings.Split(localAddr.IP.String(),":")[0]
+	fmt.Println("对外Ip是", localAddr.IP, "对外Port是", localAddr.Port)
+	ip = strings.Split(localAddr.IP.String(), ":")[0]
 	return
 }
 
-func main(){
-	ip,err :=GetOutboundIP()
-	if err != nil{
+func main() {
+	ip, err := GetOutboundIP()
+	if err != nil {
 		fmt.Println(err.Error())
+		return
 	}
 	fmt.Println(ip)
 }
