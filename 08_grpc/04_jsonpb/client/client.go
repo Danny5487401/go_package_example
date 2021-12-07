f92f610e3cc22824fe82672a12baf70360712d79
xiaxin <xiaxin@qimao.com>
2021-12-07 15:21:02 +0800

    feat(protobuf): jsonpb序列化指针为Null时的处理情况

--- 08_grpc/04_jsonpb/client/client.go
+++ 08_grpc/04_jsonpb/client/client.go
@@ -4,10 +4,10 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"os"
-
 	"github.com/golang/protobuf/jsonpb"
+	proto2 "github.com/golang/protobuf/proto"
 	"google.golang.org/grpc"
+	"os"
 
 	"go_grpc_example/08_grpc/04_jsonpb/proto"
 )
@@ -36,32 +36,39 @@ func main() {
 	}
 	//方式一:打印结果
 	//fmt.Println(r)
-
-	/*
-			Id:12  Phone:"15112810201"
-		问题
-			因为server未返回age字段，所以没有age。在某些情况下对前端也是不太友好的，尤其是APP客户端，更需要明确的json响应字段结构
-		解决方式
-		1。直接修改经过protoc生成的member.pb.go文件代码，删除掉不希望被忽略的字段tag标签中的omitempty即可，
-			但是*.pb.go一般我们不建议去修改它，而且我们会经常去调整grpc微服务协议中的方法或者字段内容，这样每次protoc之后，
-			都需要我们去修改，这显然是不太现实的，因此就有了第二种办法；
-		2。通过grpc官方库中的jsonpb来实现,官方在它的设定中有一个结构体用来实现protoc buffer转换为JSON结构，并可以根据字段来配置转换的要求，
-			结构体如下
-			// Marshaler is a configurable object for converting between
-			// protocol buffer objects and a JSON representation for them.
-			type Marshaler struct {
-				// 是否将枚举值设定为整数，而不是字符串类型.
-				EnumsAsInts bool
-				// 是否将字段值为空的渲染到JSON结构中
-				EmitDefaults bool
-				//缩进每个级别的字符串
-				Indent string
-				//是否使用原生的proto协议中的字段
-				OrigName bool
-			}
-	*/
-
 	// 方式二：Marshaler使用
+	MarshalData(r)
+
+	UnmarshalPbData()
+
+}
+
+/*
+		Id:12  Phone:"15112810201"
+	问题
+		因为server未返回age字段，所以没有age。在某些情况下对前端也是不太友好的，尤其是APP客户端，更需要明确的json响应字段结构
+	解决方式
+	1。直接修改经过protoc生成的member.pb.go文件代码，删除掉不希望被忽略的字段tag标签中的omitempty即可，
+		但是*.pb.go一般我们不建议去修改它，而且我们会经常去调整grpc微服务协议中的方法或者字段内容，这样每次protoc之后，
+		都需要我们去修改，这显然是不太现实的，因此就有了第二种办法；
+	2。通过grpc官方库中的jsonpb来实现,官方在它的设定中有一个结构体用来实现protoc buffer转换为JSON结构，并可以根据字段来配置转换的要求，
+		结构体如下
+		// Marshaler is a configurable object for converting between
+		// protocol buffer objects and a JSON representation for them.
+		type Marshaler struct {
+			// 是否将枚举值设定为整数，而不是字符串类型.
+			EnumsAsInts bool
+			// 是否将字段值为空的渲染到JSON结构中
+			EmitDefaults bool
+			//缩进每个级别的字符串
+			Indent string
+			//是否使用原生的proto协议中的字段
+			OrigName bool
+		}
+*/
+
+func MarshalData(r proto2.Message) {
+
 	var (
 		_buffer bytes.Buffer
 	)
@@ -74,9 +81,12 @@ func main() {
 	//调用此方法实现转换
 	jsonpbMarshaler.Marshal(&_buffer, r)
 	jsonCnt := _buffer.Bytes()
+	//发送给前端: {"Id":12,"Phone":"15112810201","Age":0,"data":null}  注意当data字段为空指针，会返回null给前端
 	fmt.Println("发送给前端:", string(jsonCnt))
 	// 会把空数值打印处理，方便返回给前端
+}
 
+func UnmarshalPbData() {
 	// Unmarshaler使用:转换成pb对象
 	// school没有这字段，Age为0
 	mockData := `{"Id":12,"Phone":"15112810201","Age":0,"school":"Beijing"}`
@@ -85,11 +95,11 @@ func main() {
 
 	memberRsp := proto.MemberResponse{}
 	unmarshaler := jsonpb.Unmarshaler{
-		AllowUnknownFields: true, //允许忽略未知字段，如school
+		AllowUnknownFields: true, //允许忽略未知字段，如school字段不存在
 	}
 	if err := unmarshaler.Unmarshal(bufferObj, &memberRsp); err != nil {
 		fmt.Println("jsonpb UnmarshalString fail: ", err)
 		os.Exit(0)
 	}
-	fmt.Printf("member info pb: %+v", memberRsp.String())
+	fmt.Printf("member info pb反序列化: %+v", memberRsp.String())
 }
--- 08_grpc/scripts/proto.sh
+++ 08_grpc/scripts/proto.sh
@@ -5,6 +5,6 @@
 # protoc --proto_path=${GOPATH}/src:. --gogofaster_out=plugins=grpc:. 04_jsonpb/proto/member.proto
 
 #protobuf
-protoc  --gofast_out=plugins=grpc:./01_grpc_helloworld/proto ./01_grpc_helloworld/proto/helloworld.proto
-protoc-go-inject-tag -input=./01_grpc_helloworld/proto/helloworld.pb.go
+protoc  --gofast_out=plugins=grpc:./04_jsonpb/proto ./04_jsonpb/proto/member.proto
+protoc-go-inject-tag -input=./04_jsonpb/proto/member.pb.go
 
